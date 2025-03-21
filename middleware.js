36c6112e188cb16a25767f157f271f7ab278fb7d
Md Adil Hossain <mdadilhossain2006@gmail.com>
2025-03-21 23:31:35 +0600

    Alignment fix 6

--- middleware.js
+++ middleware.js
@@ -5,18 +5,9 @@ export { default } from 'next-auth/middleware';
 
 export async function middleware(req) {
 	const token = await getToken({ req });
-	const resp = await fetch(`${process.env.NEXTAUTH_URL}/api/signout`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			_id: token?.mongoId,
-		}),
-	});
 
-	if ((token?.role !== 'admin' && token?.role !== 'superAdmin') || !resp.ok) {
-		return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/signout`);
+	if (token?.role !== 'admin' && token?.role !== 'superAdmin') {
+		return NextResponse.redirect(process.env.NEXTAUTH_URL);
 	}
 }
 
